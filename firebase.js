2302c46d15f28463038d77a48e7e989348235c64
Brightwell-Dlamini <dlaminibrightwell@gmail.com>
2022-02-18 23:18:20 +0200

    Header: Finished

---
+++ firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCnq0TDX6opRKb7zfkWM4gux-POz8-Fc6k",
+  authDomain: "linkedin-clone-4ec4c.firebaseapp.com",
+  projectId: "linkedin-clone-4ec4c",
+  storageBucket: "linkedin-clone-4ec4c.appspot.com",
+  messagingSenderId: "573963348534",
+  appId: "1:573963348534:web:59da3b3f25625281618697",
+};
---
+++ src/components/Header.js
@@ -0,0 +1,33 @@
+import React from "react";
+import "./Header.css";
+import SearchIcon from "@mui/icons-material/Search";
+import HomeIcon from "@mui/icons-material/Home";
+import HeaderOption from "./HeaderOption";
+import SupervisorAccountIcon from "@mui/icons-material/SupervisorAccount";
+import BusinessCenterIcon from "@mui/icons-material/BusinessCenter";
+import ChatIcon from "@mui/icons-material/Chat";
+import NotificationsIcon from "@mui/icons-material/Notifications";
+
+function Header() {
+  return (
+    <div className="header">
+      <div className="header_left">
+        <img src="logo192.png" alt="logo linked in" />
+        <div className="header_search">
+          <SearchIcon />
+          <input type="text" name="" />
+        </div>
+      </div>
+      <div className="header_right">
+        <HeaderOption Icon={HomeIcon} title="Home" />
+        <HeaderOption Icon={SupervisorAccountIcon} title="My Network" />
+        <HeaderOption Icon={BusinessCenterIcon} title="Jobs" />
+        <HeaderOption Icon={ChatIcon} title="Messaging" />
+        <HeaderOption Icon={NotificationsIcon} title="Notifications" />
+        <HeaderOption avatar="avatar.png" title="Me" />
+      </div>
+    </div>
+  );
+}
+
+export default Header;
